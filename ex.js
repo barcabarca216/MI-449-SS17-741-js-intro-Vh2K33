c966a0a99154831f39266c5b45d17d8005339826
Evan <cato9546@gmail.com>
2017-01-29 23:59:02 -0500

    revised JS

--- ex.js
+++ ex.js
@@ -1,4 +1,24 @@
-function play(element){
-   var audio = document.getElementById(element);
-   audio.play();
-}
+var myAudio = new Audio("snare.wav");
+var myAudio2 = new Audio("tom.wav");
+var myAudio3 = new Audio("boom.wav");
+
+
+var buttonElement = document.getElementById("snare")
+buttonElement.addEventListener('click', function () {
+myAudio.play();})
+buttonElement.addEventListener('mouseover', function () {
+myAudio.play();})
+
+
+var buttonElement = document.getElementById("tom")
+buttonElement.addEventListener('click', function () {
+myAudio2.play();})
+buttonElement.addEventListener('mouseover', function () {
+myAudio2.play();})
+
+
+var buttonElement = document.getElementById("boom")
+buttonElement.addEventListener('click', function () {
+myAudio3.play();})
+buttonElement.addEventListener('mouseover', function () {
+myAudio3.play();})
